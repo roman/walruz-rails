1b568b634db31491d17d21a66a1426c874175b1b
Roman Gonzalez <romanandreg@gmail.com>
2009-08-21 15:50:02 -0700

    Fixing an issue on the walruz policy generator

--- rails_generators/walruz_policy_generator.rb
+++ rails_generators/walruz_policy_generator.rb
@@ -1,4 +1,5 @@
 class WalruzPolicyGenerator < Rails::Generator::NamedBase
+  default_options :test => 'rspec'
   
   def manifest
     record do |m|
@@ -7,17 +8,17 @@ class WalruzPolicyGenerator < Rails::Generator::NamedBase
       m.template 'policy.rb', File.join('lib/walruz/policies', class_path, "#{file_name}.rb")
     
     
-      if options[:test]
+      case options[:test]
+      when 'rspec'
         # we generate the rSpec stub
         m.directory 'spec'
         m.directory 'spec/policies'
         m.template 'policy_spec.rb', File.join("spec/policies", "#{file_name}_spec.rb")
-      else
+      when 'testunit'
         m.directory 'test'
         m.directory 'test/policies'
         m.template 'policy_test.rb', File.join("test/policies", "#{file_name}_test.rb")
       end
-    
     end
   end
   
@@ -26,8 +27,8 @@ class WalruzPolicyGenerator < Rails::Generator::NamedBase
   def add_options!(opt)
     opt.separator ''
     opt.separator 'Options:'
-    opt.on('--spec', 'Add an rSpec test to the spec folder (DEFAULT)') { |v| options[:spec] = true }
-    opt.on('--test', 'Add a Test::Unit file to the test folder') { |v| options[:test] = true }
+    opt.on('--spec', 'Add a rSpec file to the spec folder') { options[:test] = 'rspec' }
+    opt.on('--test', 'Add a Test::Unit file to the test folder') { options[:test] = 'testunit' }
   end  
   
   def banner
